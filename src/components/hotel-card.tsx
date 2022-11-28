a010d470b2cb2020b1fd331a7b35c6349188dd67
tatasulysta <tatasulystaaa@gmail.com>
2022-11-28 21:44:12 +0700

    feat: hotel card

---
+++ src/components/hotel-card.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Image, Text, View, TouchableOpacity } from 'react-native-ui-lib';
+// import { screens } from '../screens';
+// import { Props as SampleProps } from './_screen-sample';
+import { colors } from '../utils/color';
+
+export const HotelCard = () => {
+  //   const push = () =>
+  //     screens.push<SampleProps>(componentId, 'Sample', { type: 'push' });
+  return (
+    <TouchableOpacity style={[styles.container, styles.elevation]}>
+      <Image
+        source={require('../images/background2.jpg')}
+        style={styles.image}
+      />
+      <View style={styles['information-container']}>
+        <View>
+          <Text style={styles['text-brand']}>Blue Dreams Hotel</Text>
+          <Text style={styles['text-category']}>Bali</Text>
+        </View>
+        <View>
+          <Text style={styles['text-price']}>$199</Text>
+          <Text style={styles['text-status']}>/per night</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    width: '99%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 160,
+    backgroundColor: 'white',
+    borderRadius: 7,
+  },
+  image: {
+    flex: 1,
+    width: '99%',
+    height: 120,
+    borderTopLeftRadius: 7,
+    borderTopRightRadius: 7,
+  },
+  'text-brand': {
+    color: 'black',
+    fontSize: 17,
+    fontWeight: '500',
+  },
+  'text-price': {
+    fontSize: 20,
+    color: colors.blue,
+    fontWeight: '500',
+  },
+  'text-category': {
+    color: 'black',
+  },
+  'text-status': {
+    color: '#808080',
+  },
+  'information-container': {
+    width: '99%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderBottomEndRadius: 7,
+    borderBottomLeftRadius: 7,
+  },
+  elevation: {
+    shadowColor: colors.blue,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 7,
+
+    elevation: 3,
+  },
+});
