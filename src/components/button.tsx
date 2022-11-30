4dde9a3b8bb07169082309bbf1a8be018b8bca51
tatasulysta <tatasulystaaa@gmail.com>
2022-11-30 16:11:36 +0700

    feat: login functionality and edit profile

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,22 +1,41 @@
-import React from 'react';
-import { View, Text, MarginModifiers } from 'react-native-ui-lib';
-import { Bounceable } from 'rn-bounceable';
+import React, { PropsWithChildren } from 'react';
+import { StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Text, MarginModifiers } from 'react-native-ui-lib';
+// import { Bounceable } from 'rn-bounceable';
+import { colors } from '../utils/color';
 
 type Props = MarginModifiers & {
   label?: string;
   onPress?: PureFunc;
 };
 
-export const BButton: React.FC<Props> = ({ label, onPress, ...modifiers }) => {
+export const Button: React.FC<PropsWithChildren<Props>> = ({
+  children,
+  onPress,
+}) => {
   return (
-    <View {...modifiers}>
-      <Bounceable onPress={onPress}>
-        <View center bg-secondary padding-s3 br50>
-          <Text text65M primary>
-            {label}
-          </Text>
-        </View>
-      </Bounceable>
-    </View>
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={{ ...styles.text, ...styles.buttonText }}>{children}</Text>
+    </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  text: {
+    color: colors.blue,
+  },
+  button: {
+    color: colors.blue,
+    backgroundColor: colors.yellow,
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 5,
+    width: '90%',
+  },
+  buttonText: {
+    fontWeight: '500',
+    fontSize: 15,
+    textAlign: 'center',
+  },
+});
--- src/screens/settings.tsx
+++ src/screens/settings.tsx
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { ScrollView } from 'react-native';
-import { Text, View, SegmentedControl, Colors } from 'react-native-ui-lib';
+import { ScrollView, StyleSheet, TextInput, Dimensions } from 'react-native';
+import {
+  Text,
+  View,
+  SegmentedControl,
+  Colors,
+  RadioButton,
+  RadioGroup,
+} from 'react-native-ui-lib';
 import { ScreenComponent } from 'rnn-screens';
 import { observer } from 'mobx-react';
 
@@ -19,6 +26,13 @@ import { screens } from '.';
 import { navButtons } from '../services/navigation/buttons';
 import { useNavigationButtonPress } from 'react-native-navigation-hooks/dist';
 import { useServices } from '../services';
+import { useDispatch, useSelector } from 'react-redux';
+import { editData, logout } from '../redux/user';
+import { Button } from '../components/button';
+import { RootState } from '../redux/reducer';
+import { colors } from '../utils/color';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { iOSUIKit } from 'react-native-typography';
 
 export const Settings: ScreenComponent = observer(({ componentId }) => {
   const { ui } = useStores();
@@ -27,6 +41,16 @@ export const Settings: ScreenComponent = observer(({ componentId }) => {
   // State
   const [appearance, setAppearance] = useState(ui.appearance);
   const [language, setLanguage] = useState(ui.language);
+  const dispatch = useDispatch();
+  const state = useSelector((state: RootState) => state.login);
+  const [user, setUser] = useState(state.user);
+  const [editable, setEditable] = useState(false);
+
+  useEffect(() => {
+    if (!state.isLogin) {
+      screens.push(componentId, 'Login', { type: 'push' });
+    }
+  }, [state]);
 
   // Computed
   const unsavedChanges =
@@ -65,9 +89,117 @@ export const Settings: ScreenComponent = observer(({ componentId }) => {
   };
   useNavigationButtonPress(handleSave, componentId, navButtons.save.id);
 
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
+  const handleSaveData = () => {
+    dispatch(editData(user));
+  };
+
   return (
     <View flex bg-bgColor>
       <ScrollView contentInsetAdjustmentBehavior='always'>
+        <View>
+          <View
+            style={{
+              paddingHorizontal: 20,
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Text style={iOSUIKit.title3Emphasized}>Profile</Text>
+            <TouchableOpacity
+              onPress={() => {
+                setEditable((state) => !state);
+                state && handleSaveData();
+              }}
+              style={styles.editButton}
+            >
+              <Text style={styles.text}>{editable ? 'Save' : 'Edit'}</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View
+            style={{
+              paddingHorizontal: 25,
+            }}
+          >
+            <View>
+              <Text style={iOSUIKit.subhead}>Email</Text>
+              <TextInput
+                value={user.email}
+                editable={editable}
+                placeholder='email'
+                style={styles.input}
+                onChangeText={(text: any) =>
+                  setUser((currentVal: any) => ({ ...currentVal, email: text }))
+                }
+              />
+            </View>
+            <View>
+              <Text style={iOSUIKit.subhead}>First Name</Text>
+              <TextInput
+                value={user.firstName}
+                editable={editable}
+                placeholder='First Name'
+                style={styles.input}
+                onChangeText={(text: any) =>
+                  setUser((currentVal: any) => ({
+                    ...currentVal,
+                    firstName: text,
+                  }))
+                }
+              />
+            </View>
+            <View>
+              <Text style={iOSUIKit.subhead}>Last Name</Text>
+              <TextInput
+                value={user.lastName}
+                editable={editable}
+                placeholder='Last Name'
+                style={styles.input}
+                onChangeText={(text: any) =>
+                  setUser((currentVal: any) => ({
+                    ...currentVal,
+                    lastName: text,
+                  }))
+                }
+              />
+            </View>
+            <View>
+              <Text style={iOSUIKit.subhead}>Gender</Text>
+              <RadioGroup
+                initialValue={true}
+                onValueChange={(value: any) => {
+                  console.log(value);
+                  setUser((user: Boolean) => ({ ...user, gender: value }));
+                  console.log(user);
+                }}
+                style={styles.radio}
+              >
+                <View style={styles.radioInput}>
+                  <RadioButton
+                    value={false}
+                    label={'Female'}
+                    color={colors.blue}
+                    disabled={!editable}
+                  />
+                </View>
+                <View style={styles.radioInput}>
+                  <RadioButton
+                    value={true}
+                    label={'Male'}
+                    color={colors.blue}
+                    disabled={!editable}
+                  />
+                </View>
+              </RadioGroup>
+            </View>
+          </View>
+        </View>
+
         <Section title={'UI'}>
           <View paddingV-s1>
             <Row>
@@ -105,9 +237,52 @@ export const Settings: ScreenComponent = observer(({ componentId }) => {
                 onChangeIndex={handleLanguageIndexChange}
               />
             </Row>
+            <Button onPress={handleLogout}>Logout</Button>
           </View>
         </Section>
       </ScrollView>
     </View>
   );
+});
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    width: '90%',
+    borderTopColor: 'white',
+    borderRightColor: 'white',
+    borderLeftColor: 'white',
+    borderWidth: 1.5,
+    borderBottomColor: colors.grayBorder,
+    color: 'black',
+    fontWeight: '500',
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  radio: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  radioInput: {
+    marginRight: 10,
+    marginTop: 10,
+  },
+  text: {
+    color: colors.blue,
+  },
+  editButton: {
+    borderColor: colors.blue,
+    backgroundColor: 'white',
+    borderWidth: 2,
+    justifyContent: 'center',
+    padding: 6,
+    borderRadius: 5,
+    width: 50,
+    alignItems: 'center',
+  },
+  profile: {
+    position: 'absolute',
+  },
 });
